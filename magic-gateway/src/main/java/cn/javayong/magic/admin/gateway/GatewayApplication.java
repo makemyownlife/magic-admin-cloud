0cbe722f3b9c77d3418f1bafa1a8d96c35207c30
zhangyong <zhangyong7120180@163.com>
2024-09-07 23:16:37 +0800

    测试网关main函数

---
+++ magic-gateway/src/main/java/cn/javayong/magic/admin/gateway/GatewayApplication.java
@@ -0,0 +1,9 @@
+package cn.javayong.magic.admin.gateway;
+
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello ,world");
+    }
+
+}
